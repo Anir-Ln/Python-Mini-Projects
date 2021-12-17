b5c43c7d160735c362f79afd8631cb9735cefc2f
anir4 <Howmed4@gmail.com>
2021-12-18 00:16:58 +0100

    Update snakeGame.py

--- snakeGame.py
+++ snakeGame.py
@@ -7,8 +7,12 @@ import random
 #variables
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 400
-BLOCK_LENGHT = 10
+SCREEN_AREA = SCREEN_WIDTH * SCREEN_HEIGHT # 400 * 600 = 240000
+BLOCK_LENGHT = 20
 BLOCK_SIZE = BLOCK_LENGHT * BLOCK_LENGHT
+TIME_CHANGE = 10
+OBSTACLES_NUMBER = random.randint(1, 100);
+BLOCKS_NUMBER = (SCREEN_AREA) // BLOCK_SIZE
 
 
 #colors
@@ -24,21 +28,26 @@ foodY = SCREEN_HEIGHT//2
 
 
 
+obstacles = [(random.randint(0, SCREEN_WIDTH // BLOCK_LENGHT - 1) * BLOCK_LENGHT, random.randint(0, SCREEN_HEIGHT // BLOCK_LENGHT - 1) * BLOCK_LENGHT) for _ in range(OBSTACLES_NUMBER)] 
+
+
 def newFood():
 	# should use global keyword to modify position variables
 	global foodX, foodY
-	foodX = random.randint(0, SCREEN_WIDTH // BLOCK_LENGHT) * BLOCK_LENGHT
-	foodY = random.randint(0, SCREEN_HEIGHT // BLOCK_LENGHT) * BLOCK_LENGHT
+	foodX = random.randint(0, SCREEN_WIDTH // BLOCK_LENGHT - 1) * BLOCK_LENGHT
+	foodY = random.randint(0, SCREEN_HEIGHT // BLOCK_LENGHT - 1) * BLOCK_LENGHT
 	print(f"({foodX}, {foodY})")
 
 
 class Snake(object):
 	"""docstring for Snake"""
-	def __init__(self, disp, x, y, length, direction, activateBorder):
+	x = [0] * BLOCKS_NUMBER
+	y = [0] * BLOCKS_NUMBER
+	def __init__(self, disp, x0, y0, length, direction, activateBorder):
 		super(Snake, self).__init__()
 		self.disp = disp
-		self.x = x
-		self.y = y
+		self.x[0] = x0
+		self.y[0] = y0
 		self.length = length
 		self.direction = direction
 		self.activateBorder = activateBorder
@@ -65,14 +74,19 @@ class Snake(object):
 	def draw(self):
 		for i in range(self.length):
 			pygame.draw.rect(self.disp, black, [self.x[i], self.y[i], BLOCK_LENGHT, BLOCK_LENGHT])
+		
+		# draw obstacles
+		for ob in obstacles:
+			pygame.draw.rect(self.disp, blue, [ob[0], ob[1], BLOCK_LENGHT, BLOCK_LENGHT])
+
 		# draw food
 		# print(f"--{foodX}, {foodY}--")
 		pygame.draw.rect(self.disp, red, [foodX, foodY, BLOCK_LENGHT, BLOCK_LENGHT])
 
 	def eat(self):
 		if foodX == self.x[0] and foodY == self.y[0]:
-			self.x.append(0)
-			self.y.append(0)
+			# self.x.append(0)
+			# self.y.append(0)
 			self.length += 1
 			#new food position
 			newFood()
@@ -81,8 +95,14 @@ class Snake(object):
 
 	def checkCollisions(self):
 		# out of borders
-		if self.x[0] >= SCREEN_WIDTH or self.x[0] < 0 or self.y[0] >= SCREEN_HEIGHT or self.y[0] < 0:
-			return True
+		if not self.activateBorder:
+			if self.x[0] >= SCREEN_WIDTH or self.x[0] < 0 or self.y[0] >= SCREEN_HEIGHT or self.y[0] < 0:
+				return True
+
+		# obstacles
+		for ob in obstacles:
+			if ob[0] == self.x[0] and ob[1] == self.y[0]:
+				return True
 
 		# head and body collision
 		for i in range(1, self.length):
@@ -105,7 +125,7 @@ if __name__ == '__main__':
 
 	running = True
 
-	snake = Snake(disp, [SCREEN_WIDTH//2], [SCREEN_HEIGHT//2], 1, 'R', True)
+	snake = Snake(disp, SCREEN_WIDTH//2, SCREEN_HEIGHT//2, 1, 'R', True)
 
 	while running:
 		for event in pygame.event.get():
@@ -126,16 +146,19 @@ if __name__ == '__main__':
 					foodX = snake.x[0]
 					foodY = snake.y[0]
 					snake.eat()
+				break
+				# wihout break the game can be hacked by pressing 2 arrow keys at the same time to switch direction from up <-> down or left <-> right
 
 		disp.fill(white)
 		snake.move()
 		snake.draw()
 		if snake.checkCollisions():
 			print(f"Collision at <{snake.x[0], snake.y[0]}>")
+			snake.length = 1
 		snake.eat()
 		pygame.display.update()
 
-		clock.tick(BLOCK_LENGHT)
+		clock.tick(TIME_CHANGE)
 
 	
 
